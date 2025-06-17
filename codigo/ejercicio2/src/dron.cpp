cfdd4b13595679a2579d0dcf13b4f22d2e8bcfc7
belencasas <mcasas@udesa.edu.ar>
2025-06-17 15:10:39 -0300

    listoo

--- codigo/ejercicio2/src/dron.cpp
+++ codigo/ejercicio2/src/dron.cpp
@@ -1,16 +1,16 @@
 #include "../include/dron.h"
 
-// Default mutexes for default constructor
+// mutex por defecto compartidos por los drones creados con el constructor por defecto
 static mutex default_mutex_izq;
 static mutex default_mutex_der;
 Dron::Dron() : id(0), zona_izq(default_mutex_izq), zona_der(default_mutex_der) {}
 
-Dron::Dron(int id, mutex& izq, mutex& der) 
-    : id(id), zona_izq(izq), zona_der(der) {}
+Dron::Dron(int id, mutex& izq, mutex& der) : id(id), zona_izq(izq), zona_der(der) {}
+
 
 void Dron::operator()(){
     {
-        // para que no se superpongan los mensajes 
+        // protege el acceso a cout para que no se superpongan los mensajes 
         lock_guard<mutex> lock_cout(mutex_cout);
         cout << "Dron " << id << " esperando para despegar" << endl;
     }
@@ -27,6 +27,7 @@ void Dron::operator()(){
         cout << "Dron " << id << " despegando..." << endl;  
     }
 
+    //simulacion del tiempo de despegue 
     this_thread::sleep_for(chrono::seconds(5));
 
     {
